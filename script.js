69df8a36cd0eabe7a4d74c114d1aa67a029fef12
darialiit <solomkina.daria@gmail.com>
2023-11-21 09:51:08 -0300

    style cards

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ function Book(title, author, pages,read) {
   this.read = read;
 }
 
-let book1 = new Book("Crime and Punishment", "F. Dostoevsky", 527, true);
-let book2 = new Book("War and Peace", "L. Tolstoy", 1225, true);
-
-myLibrary.push(book1, book2)
 
 function addBookToLibrary() {
   // do stuff here
