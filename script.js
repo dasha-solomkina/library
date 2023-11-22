0e06ad3e0ddf9eda6fb89cceb84b519cff8142c5
darialiit <solomkina.daria@gmail.com>
2023-11-22 19:44:12 -0300

    add status switcher

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const addBookBtn = document.querySelector(".add-book");
-const readBtn = document.querySelector(".read");
+const readBtn = document.querySelectorAll(".read");
 const removeBtn = document.querySelector(".remove");
-
+const form = document.querySelector("#myForm");
 
 const myLibrary = [];
-
-  
 
 
 function Book(title, author, pages,read) {
@@ -15,7 +13,37 @@ function Book(title, author, pages,read) {
   this.read = read;
 }
 
+// open the pop up to add a book when button clicked
+addBookBtn.addEventListener("click", () => {
+  const popup = document.querySelector("#hidden");
+  popup.removeAttribute("id");
+})
 
-function addBookToLibrary() {
-  // do stuff here
-}
+// read vs unread
+
+readBtn.forEach(function(btn) {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("notread")) {
+      btn.classList.remove("notread");
+    } else {
+      btn.classList.add("notread");
+    };
+  });
+});
+
+
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+  myLibrary.push(new Book(title, author, pages, read));
+
+})
+
+
+
+// function addBookToLibrary() {
+  
+// }
