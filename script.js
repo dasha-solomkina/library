428cf07d646f67978f3eafd7117fa5796f8e6b99
darialiit <solomkina.daria@gmail.com>
2023-11-25 22:46:17 -0300

    add to library function

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-const addBookBtn = document.querySelector(".add-book");
+window.addEventListener('click', (e)=> {
+  // console.log(e.target.parentNode.id)
+  if (e.target.classList.contains("read")) {
+    unRead(e.target)
+  }
+  if (e.target.classList.contains("remove")) {
+    removeCard(e.target);
+  }
+
+});
+
+function removeCard(element) {
+  const divRemove = document.getElementById(element.parentNode.id);
+  myLibrary.splice(divRemove.id, 1);
+  container.removeChild(divRemove);
+  updateId();
+}
+
+function unRead(btn) {
+  if (btn.classList.contains("notread")) {
+    btn.classList.remove("notread");
+  } else {
+    btn.classList.add("notread");
+  };
+};
+
+function updateId() {
+  const allCards = document.querySelectorAll(".card");
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].id = i;
+  };
+};
+
+
 const readBtn = document.querySelectorAll(".read");
 const removeBtn = document.querySelector(".remove");
+
+const addBookBtn = document.querySelector(".add-book");
+
 const form = document.querySelector("#myForm");
 const container = document.querySelector(".container");
 const popup = document.querySelector("#hidden");
 
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages,read) {
   this.title = title;
@@ -22,15 +58,15 @@ addBookBtn.addEventListener("click", () => {
 
 // read vs unread
 
-readBtn.forEach(function(btn) {
-  btn.addEventListener("click", () => {
-    if (btn.classList.contains("notread")) {
-      btn.classList.remove("notread");
-    } else {
-      btn.classList.add("notread");
-    };
-  });
-});
+// readBtn.forEach(function(btn) {
+//   btn.addEventListener("click", () => {
+//     if (btn.classList.contains("notread")) {
+//       btn.classList.remove("notread");
+//     } else {
+//       btn.classList.add("notread");
+//     };
+//   });
+// });
 
 
 form.addEventListener("submit", () => {
@@ -55,12 +91,13 @@ function addCard(arr) {
   const newRead = document.createElement("button");
   const newRemove = document.createElement("button");
   newDiv.classList.add("card");
+  newDiv.setAttribute('id', arr.length-1);
   newTitle.classList.add("title");
   newAuthor.classList.add('author');
   newPages.classList.add('pages');
   newRead.classList.add('read');
   newRemove.classList.add("remove");
-  newRemove.setAttribute('id', arr.length-1);
+  // newRemove.setAttribute('id', arr.length-1);
   newTitle.textContent = arr[arr.length-1].title;
   newAuthor.textContent = arr[arr.length-1].author;
   newPages.textContent = arr[arr.length-1].pages;
